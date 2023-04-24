0cb3c051ff0089508f22f15293d4ad673854275d
Anirudh Chintha <anirudhchintha95@gmail.com>
2023-04-24 09:22:54 -0400

    User and Connection Modal Changes

--- server/models/user.js
+++ server/models/user.js
@@ -1,141 +1,190 @@
 import { model, Schema } from "mongoose";
 import { GENDERS } from "../constants.js";
+import * as validations from "../validators/helpers.js";
 
 const PreferenceSchema = new Schema({
-    smoking: {
-        type: Boolean,
-        default: false,
+  smoking: {
+    type: Boolean,
+  },
+  drinking: {
+    type: Boolean,
+  },
+  pets: {
+    type: Boolean,
+  },
+  rent: {
+    type: Object,
+    validate: {
+      validator: function (v) {
+        if (Object.keys(v).length === 0) return true;
+        const hasMin = typeof v.min === "number";
+        const hasMax = typeof v.max === "number";
+        const hasExact = typeof v.exact === "number";
+
+        if (hasExact) {
+          try {
+            validations.validateNumber(v.exact, "exact rent", {
+              min: 1,
+            });
+          } catch (error) {
+            return false;
+          }
+        }
+
+        if (hasMin) {
+          try {
+            validations.validateNumberRange(v.min, "min rent", {
+              min: 1,
+            });
+          } catch (error) {
+            return false;
+          }
+        }
+        if (hasMax) {
+          try {
+            validations.validateNumberRange(v.max, "max rent", {
+              min: 1,
+            });
+          } catch (error) {
+            return false;
+          }
+        }
+
+        return hasMin && hasMax ? v.min <= v.max : true;
+      },
+      message: () => `Rent range is not valid!`,
     },
-    drinking: {
-        type: Boolean,
-        default: false,
+  },
+  age: {
+    type: Object,
+    validate: {
+      validator: function (v) {
+        if (Object.keys(v).length === 0) return true;
+        const hasMin = typeof v.min === "number";
+        const hasMax = typeof v.max === "number";
+
+        if (hasMin) {
+          try {
+            validations.validateNumberRange(v.min, "min age", {
+              min: 13,
+              includeMin: true,
+            });
+          } catch (error) {
+            return false;
+          }
+        }
+        if (hasMax) {
+          try {
+            validations.validateNumberRange(v.max, "max age", {
+              min: 13,
+              max: 100,
+              includeMin: true,
+            });
+          } catch (error) {
+            return false;
+          }
+        }
+
+        return hasMin && hasMax ? v.min <= v.max : true;
+      },
+      message: () => `Age range is not valid!`,
     },
-    pets: {
-        type: Boolean,
-        default: false,
+  },
+  genders: {
+    type: Array,
+    validate: {
+      validator: function (v) {
+        return v.every((g) => Object.values(GENDERS).includes(g));
+      },
+      message: (props) => `${props.value} does not have valid genders!`,
     },
-    rent: {
-        type: Object,
-        min: {
-            type: Number,
-            default: 0,
-        },
-        max: {
-            type: Number,
-            default: 0,
-        },
-        exact: {
-            type: Number,
-            default: 0,
-        },
-        default: { min: 0, max: 0, exact: 0 },
-    },
-    age: {
-        type: Object,
-        min: {
-            type: Number,
-            default: 0,
-        },
-        max: {
-            type: Number,
-            default: 0,
-        },
-        default: { min: 0, max: 0 },
-    },
-    gender: {
-        type: Array,
-        validate: {
-            validator: function (v) {
-                return v.every((g) => Object.values(GENDERS).includes(g));
-            },
-            message: (props) => `${props.value} does not have valid genders!`,
-        },
-        required: true,
-    },
+  },
 });
 
 const UserSchema = new Schema(
-    {
-        firstName: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        lastName: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true,
-        },
-        phone: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true,
-        },
-        encryptedPassword: {
-            type: String,
-            required: true,
-        },
-        dateOfBirth: {
-            type: Date,
-            required: true,
-        },
-        gender: {
-            type: String,
-            validate: {
-                validator: function (v) {
-                    return Object.values(GENDERS).includes(v);
-                },
-                message: (props) => `${props.value} is not a valid gender!`,
-            },
-            required: true,
-        },
-        location: {
-            city: {
-                type: String,
-                required: true,
-                trim: true,
-            },
-            state: {
-                type: String,
-                required: true,
-                trim: true,
-            },
-        },
-        role: {
-            type: String,
-            enum: ["user", "admin"],
-            default: "user",
-        },
-        preferences: {
-            type: PreferenceSchema,
-            required: true,
-        },
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    {
-        timestamps: true,
-        methods: {
-            async verifyPassword(password) {
-                try {
-                    return new PasswordService(password).verify(
-                        this.encryptedPassword
-                    );
-                } catch (error) {
-                    return false;
-                }
-            },
-            fullName() {
-                return `${this.firstName} ${this.lastName}`;
-            },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    encryptedPassword: {
+      type: String,
+      required: true,
+    },
+    dateOfBirth: {
+      type: Date,
+      required: true,
+    },
+    gender: {
+      type: String,
+      validate: {
+        validator: function (v) {
+          return Object.values(GENDERS).includes(v);
         },
-    }
+        message: (props) => `${props.value} is not a valid gender!`,
+      },
+      required: true,
+    },
+    location: {
+      city: {
+        type: String,
+        trim: true,
+      },
+      state: {
+        type: String,
+        trim: true,
+      },
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    preferences: {
+      type: PreferenceSchema,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    methods: {
+      async verifyPassword(password) {
+        try {
+          return new PasswordService(password).verify(this.encryptedPassword);
+        } catch (error) {
+          return false;
+        }
+      },
+      fullName() {
+        return `${this.firstName} ${this.lastName}`;
+      },
+    },
+  }
 );
+
+UserSchema.index({
+  firstName: "text",
+  lastName: "text",
+  email: "text",
+});
 
 const User = model("User", UserSchema);
 
