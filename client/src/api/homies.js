e64eed162f3b9cbd750ad8cae29ff69276c63e3c
Anirudh Chintha <anirudhchintha95@gmail.com>
2023-04-24 17:09:17 -0400

    [HOM-19] My Homies FE

--- client/src/api/homies.js
+++ client/src/api/homies.js
@@ -4,3 +4,39 @@ export const fetchHomiesApi = async () => {
   const response = await axiosInstance.post("/homies");
   return response?.data?.homies || [];
 };
+
+export const fetchMyHomiesApi = async ({ connectionType, search }) => {
+  const response = await axiosInstance.post("/homies/linked", {
+    connectionType,
+    search,
+  });
+  return response?.data?.homies || [];
+};
+
+export const addUserAsFavoriteApi = async (userId) => {
+  const response = await axiosInstance.post(`/homies/${userId}/add-favorite`);
+
+  return response?.data?.user || {};
+};
+
+export const removeUserAsFavoriteApi = async (userId) => {
+  const response = await axiosInstance.post(
+    `/homies/${userId}/remove-favorite`
+  );
+
+  return response?.data?.user || {};
+};
+
+export const removeMatchedUserApi = async (userId) => {
+  const response = await axiosInstance.post(
+    `/homies/${userId}/remove-match`
+  );
+
+  return response?.data?.user || {};
+};
+
+export const reportUserApi = async (userId) => {
+  const response = await axiosInstance.post(`/homies/${userId}/report`);
+
+  return response?.data?.user || {};
+};
--- client/src/routes/MyHomies.js
+++ client/src/routes/MyHomies.js
@@ -1,7 +1,107 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Unstable_Grid2";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+
+import { fetchMyHomiesApi } from "../api/homies";
+import { CONNECTION_TYPES, CONNECTION_TYPES_TO_STATUSES } from "../contants";
+
+import { HomieCard, HomieSkeletonCard, SearchInputForm } from "../components";
+
+const connectionTypes = Object.values(CONNECTION_TYPES);
 
 const MyHomies = () => {
-  return <div>MyHomies</div>;
+  const [myHomies, setMyHomies] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState();
+
+  const [connectionTypeIdx, setConnectionTypeIdx] = React.useState(0);
+  const [search, setSearch] = React.useState("");
+
+  const handleChange = (_e, index) => {
+    setConnectionTypeIdx(index);
+  };
+
+  const fetchMyHomies = useCallback(async () => {
+    try {
+      setLoading(true);
+      setError();
+      const users = await fetchMyHomiesApi({
+        connectionType: connectionTypes[connectionTypeIdx],
+        search,
+      });
+      setMyHomies(users);
+    } catch (error) {
+      setError(
+        error?.response?.data?.error ||
+          error?.message ||
+          "Could not fetch homies"
+      );
+    } finally {
+      setLoading(false);
+    }
+  }, [connectionTypeIdx, search]);
+
+  useEffect(() => {
+    fetchMyHomies();
+  }, [fetchMyHomies]);
+
+  return (
+    <Box sx={{ width: "100%" }} marginTop={2}>
+      <Grid container rowSpacing={2}>
+        <Grid
+          xs={12}
+          md={6}
+          display="flex"
+          sx={{ justifyContent: { xs: "center", md: "flex-start" } }}
+        >
+          <Tabs value={connectionTypeIdx} onChange={handleChange}>
+            {connectionTypes.map((type) => (
+              <Tab label={type} />
+            ))}
+          </Tabs>
+        </Grid>
+        <Grid
+          xs={12}
+          md={6}
+          display="flex"
+          sx={{ justifyContent: { xs: "center", md: "flex-end" } }}
+        >
+          <SearchInputForm onSubmit={(searchTxt) => setSearch(searchTxt)} />
+        </Grid>
+      </Grid>
+      <br />
+      {loading ? (
+        <Grid container spacing={2}>
+          <Grid xs={12} sm={6} md={4} lg={3} key={0}>
+            <HomieSkeletonCard variant="small" />
+          </Grid>
+          <Grid xs={12} sm={6} md={4} lg={3} key={1}>
+            <HomieSkeletonCard variant="small" />
+          </Grid>
+        </Grid>
+      ) : error ? (
+        <>Show Error Here</>
+      ) : (
+        <Grid container spacing={2}>
+          {myHomies.map((user) => (
+            <Grid xs={12} sm={6} md={4} lg={3} key={user._id}>
+              <HomieCard
+                user={user}
+                variant="small"
+                status={
+                  CONNECTION_TYPES_TO_STATUSES[
+                    connectionTypes[connectionTypeIdx]
+                  ]
+                }
+              />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Box>
+  );
 };
 
 export default MyHomies;
