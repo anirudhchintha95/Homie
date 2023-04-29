8de55e319e4785cebb60553d08c5e63ed9d8b7c2
v-apor <apoorv.learn@gmail.com>
2023-04-29 01:29:29 -0400

    [HOM-10] Connections data functions

---
+++ server/data/connections.js
@@ -0,0 +1,48 @@
+import { isValidObjectId } from "mongoose";
+import Connection from "../models/connection.js";
+
+export const getAllConnections = async () => {
+  console.log("Hello");
+  const connections = await Connection.find({});
+  return connections;
+};
+
+export const getConnectionByCreatedForUserId = async (userId) => {
+  if (!isValidObjectId(userId))
+    throw { status: 400, message: "Error: Invalid User Id" };
+
+  const connection = await Connection.findOne(
+    { createdForUserId: userId },
+    "-__v"
+  );
+  return connection;
+};
+
+export const createConnection = async (createdForUserId, createdByUserId) => {
+  if (!isValidObjectId(createdByUserId) || !isValidObjectId(createdForUserId)) {
+    throw { status: 400, message: "Error: Invalid User Id" };
+  }
+
+  const connection = await Connection.create({
+    createdByUserId: createdByUserId,
+    createdForUserId: createdForUserId,
+  });
+
+  return connection;
+};
+
+export const getConnectionByCreatedForAndCreatedByUserId = async (
+  createdForUserId,
+  createdByUserId
+) => {
+  if (!isValidObjectId(createdForUserId) || !isValidObjectId(createdByUserId)) {
+    throw { status: 400, message: "Error: Invalid User Id" };
+  }
+
+  const connection = await Connection.findOne(
+    { createdForUserId: createdForUserId, createdByUserId: createdByUserId },
+    "-__v"
+  );
+
+  return connection ? connection : null;
+};
