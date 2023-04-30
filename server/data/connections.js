e60810490b74b13dc8a6224dec922bd2bdbf213e
v-apor <apoorv.learn@gmail.com>
2023-04-30 16:28:51 -0400

    [HOM-13] [HOM-10] Changes for both_ignored and refactor

--- server/data/connections.js
+++ server/data/connections.js
@@ -61,3 +61,86 @@ export const swapConnectionUsers = async (connection) => {
   connection.createdForUserId = temp;
   return connection.save();
 };
+
+export const removeFavorite = async (user, userBeingViewed) => {
+  try {
+    const connection = await getConnectionByCreatedForAndCreatedByUserId(
+      user,
+      userBeingViewed
+    );
+
+    if (!isValidObjectId(userBeingViewed) || !isValidObjectId(user)) {
+      throw { status: 400, message: "Error: Invalid user ID" };
+    }
+
+    if (connection) {
+      if (connection.status === "ignored") {
+        connection.status = "both_ignored";
+        await connection.save();
+        return connection;
+      } else if (connection.status === "favorite") {
+        const swappedConnection = await swapConnectionUsers(connection);
+        swappedConnection.status = "ignored";
+        swappedConnection.save();
+        return swappedConnection;
+      } else {
+        // Change this
+        throw { status: 500, message: "Invalid connection status" };
+      }
+    } else {
+      const newConnection = await createConnection(userBeingViewed, user);
+      newConnection.status = "ignored";
+      await newConnection.save();
+      return newConnection;
+    }
+  } catch (error) {
+    throw {
+      status: error.status || 500,
+      message: error.message || "Internal server error",
+    };
+  }
+};
+
+export const addFavorite = async (user, userBeingViewed) => {
+  try {
+    const connectionExists = await getConnectionByCreatedForAndCreatedByUserId(
+      user,
+      userBeingViewed
+    );
+
+    if (!isValidObjectId(userBeingViewed) || !isValidObjectId(user)) {
+      throw { status: 400, message: "Error: Invalid user ID" };
+    }
+
+    if (connectionExists) {
+      if (connectionExists.status === "favorite") {
+        // Connection already exists and is a favorite, update status to match
+        connectionExists.status = "matched";
+        await connectionExists.save();
+        return connectionExists;
+      } else if (
+        connectionExists.status === "ignored" ||
+        connectionExists.status === "both_ignored"
+      ) {
+        if (connectionExists.createdByUserId !== user) {
+          // Connection already exists and is ignored, swap createdBy and createdFor and update status to favorite
+          await swapConnectionUsers(connectionExists);
+        }
+        connectionExists.status = "favorite";
+        await connectionExists.save();
+        return connectionExists;
+      } else {
+        throw { status: 400, message: "Invalid connection status" };
+      }
+    } else {
+      // Create new connection
+      const newConnection = await createConnection(userBeingViewed, user);
+      newConnection.status = "favorite";
+      await newConnection.save();
+
+      return newConnection;
+    }
+  } catch (error) {
+    throw error;
+  }
+};
