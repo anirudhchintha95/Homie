c7f0f0139663ee2db888f4c273cd47c8faa01f7c
Anirudh Chintha <anirudhchintha95@gmail.com>
2023-05-02 23:58:39 -0400

    Create RentSlider component

---
+++ client/src/components/RentSlider.js
@@ -0,0 +1,47 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Slider from "@mui/material/Slider";
+
+function valuetext(value) {
+  return `$${value * 100}`;
+}
+
+const minDistance = 10;
+
+export default function MinimumDistanceSlider({
+  minRent,
+  maxRent,
+  onMinRentChange,
+  onMaxRentChange,
+}) {
+  const handleChange = (_, newValue, activeThumb) => {
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+
+    if (activeThumb === 0) {
+      onMinRentChange({
+        value: Math.min(newValue[0] * 100, maxRent.value - minDistance * 100),
+        error: "",
+      });
+    } else {
+      onMaxRentChange({
+        value: Math.max(newValue[1] * 100, minRent.value + minDistance * 100),
+        error: "",
+      });
+    }
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Slider
+        getAriaLabel="Rent Slider"
+        value={[minRent.value / 100, maxRent.value / 100]}
+        onChange={handleChange}
+        valueLabelDisplay="auto"
+        getAriaValueText={valuetext}
+        disableSwap
+      />
+    </Box>
+  );
+}
