6334a14787f053f490e62b4fe35c9a7d2a65f0ef
v-apor <apoorv.learn@gmail.com>
2023-05-05 13:57:06 -0400

    [HOM-31] User create-preference XSS sanitization

--- server/routes/preferences.js
+++ server/routes/preferences.js
@@ -2,14 +2,46 @@ import { Router } from "express";
 import User from "../models/user.js";
 import { preferenceData } from "../data/index.js";
 import { validatePreferencesBE } from "../validators/helpers.js";
+import xss from "xss";
 
 const router = Router();
 
 router.route("/").post(async (req, res) => {
   try {
-    const validatePref = validatePreferencesBE(req.body);
+    const {
+      smoking,
+      drinking,
+      pets,
+      city,
+      state,
+      rentMin,
+      rentMax,
+      ageMin,
+      ageMax,
+      genders,
+    } = req.body;
+
+    const cleanCity = xss(city);
+    const cleanState = xss(state);
+    const cleanGenders = req.body.genders.map((gender) => xss(gender));
+
+    const cleanReqBody = {
+      smoking,
+      drinking,
+      pets,
+      city: cleanCity,
+      state: cleanState,
+      rentMin,
+      rentMax,
+      ageMin,
+      ageMax,
+      genders: cleanGenders,
+    };
+
+    const validatePref = validatePreferencesBE(cleanReqBody);
+
     const user = await preferenceData.createPreferences(
-      req.body,
+      cleanReqBody,
       req.currentUser.email.toLowerCase()
     );
     return res.status(200).json({ user });
