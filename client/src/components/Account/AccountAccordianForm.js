522b2ee14e28a6d8374f39b3d2392951255ee0b8
parthmehta596 <parthmehta596@gmail.com>
2023-05-08 14:27:42 -0400

    Removed hyperText from DatePicker

--- client/src/components/Account/AccountAccordianForm.js
+++ client/src/components/Account/AccountAccordianForm.js
@@ -253,7 +253,6 @@ const AccountAccordianForm = ({
                   )
                 }
                 error={dob.error}
-                helperText={dob.error}
               />
             </Grid>
 
